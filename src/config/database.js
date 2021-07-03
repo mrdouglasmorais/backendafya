5bb203cf4310c253b2cb4d8e4c47c5296a23d065
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-07-03 19:22:51 -0300

    refec

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 module.exports = {
-  dialect: process.env.DATABASE_DIALECT,
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASS,
-  database: process.env.DATABASE_NAME,
+  dialect: 'postgres',
+  host: 'ec2-52-6-77-239.compute-1.amazonaws.com',
+  username: 'ptnzvkfjmuhdmm',
+  password: '1bc61e4e38b8bfd1e6068d2f2b4225044eb3bc66c80da8657647735469c1d655',
+  database: 'd2mt8jhebjl1ao',
+  port: '5432',
   logging: false,
   define: {
     timestamps: true,
