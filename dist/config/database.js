4bd49a6d699a780c5109ca69788ba41ad5fe9e64
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-07-03 19:28:56 -0300

    refec

--- dist/config/database.js
+++ dist/config/database.js
@@ -4,7 +4,6 @@
   username: 'ptnzvkfjmuhdmm',
   password: '1bc61e4e38b8bfd1e6068d2f2b4225044eb3bc66c80da8657647735469c1d655',
   database: 'd2mt8jhebjl1ao',
-  port: '5432',
   logging: false,
   define: {
     timestamps: true,
